65cf00dc443871d0cd0f285ab5f25c9064824053
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-10-07 17:29:49 +0700

    Tugas Commit Method 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,32 @@ namespace Bubble_Sort
         private int[] a= new int[20];
         //Deklarasi variabel int untuk menyimpan banyak data pada array 
         private int n;
+        // Fungsi /method untuk menerima masukan 
+        public void read ()
+        {
+            // Menerima Angka untuk menentukan banyaknya data yang disimpan pada array
+            while(true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("_______________");
+            Console.WriteLine("Masukkan Elemen Array");
+            Console.WriteLine("________________");
+
+            //Pengguna Memasukkan elemen pada array
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
         
         static void Main(string[] args)
         {
